ba0de264392300ec39f994e8e4837f4e37e500bb
choong <chungin1801@naver.com>
2019-02-08 10:40:43 +0900

    add gvim

--- scripts/install_all.sh
+++ scripts/install_all.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
-mpv.sh
-nvidia.sh
-rmbeep.sh
-utility.sh
-djv.sh
+./nvidia.sh
+./rmbeep.sh
+./trans.sh
+./utility.sh
+./djv.sh
+./code.sh
+./mpv.sh
+./rmhotcorner.sh
+./pip.sh
+./devtools.sh
+./ffmpeg.sh
+./handbrake.sh
+./openimageio.sh
 
