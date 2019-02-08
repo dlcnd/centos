7b098e3b08540dc38e151736f03911d0738aa76a
choong <chungin1801@naver.com>
2019-02-08 16:31:18 +0900

    add PySide and Numpy

--- scripts/pip.sh
+++ scripts/pip.sh
@@ -3,7 +3,8 @@ cd/ tmp
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 python get-pip.py
 pip install --user pymediainfo
-
+pip install --user PySide2
+pip install --user numpy	
 #pip install --upgrade setuptools 
 #간혹 이 작업 후 부팅 안되는 컴퓨터 발생 
 
