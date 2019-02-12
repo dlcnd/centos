01963bb6fde843375d431f306feaf1efadd01b19
choong <chungin1801@naver.com>
2019-02-12 10:53:31 +0900

    add edl, opentimelineio

--- scripts/pip.sh
+++ scripts/pip.sh
@@ -4,7 +4,18 @@ curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 python get-pip.py
 pip install --user pymediainfo
 pip install --user PySide2
-pip install --user numpy	
+pip install --user numpy
+pip install --user Cython
+ppip install --user openpyxl
+pip install --user xlsxwriter
+pip install --user xlrd
+pip install --user xlwt
+pip install --user xlutils
+pip install --user pillow 
+pip install --user timecode
+pip install --user edl
+pip install --user opentimelineio
+
 #pip install --upgrade setuptools 
 #간혹 이 작업 후 부팅 안되는 컴퓨터 발생 
 
