b0253d59999f6977bf08d317695ed079c675cacc
dlcnd <32660722+dlcnd@users.noreply.github.com>
2019-02-27 11:08:49 +0900

    Update gaffer.sh

--- scripts/user/gaffer.sh
+++ scripts/user/gaffer.sh
@@ -2,7 +2,6 @@
 if [ ! -d "/home/$USER/app" ]; then
 	mkdir /home/$USER/app
 fi
-
 cd /home/$USER/app
 wget https://github.com/ImageEngine/gaffer/releases/download/0.53.0.0/gaffer-0.53.0.0-linux.tar.gz
 tar -zxvf gaffer-0.53.0.0-linux.tar.gz
