2a0483ca2cf3f01dba452c32cf000049e2628ae0
dlcnd <32660722+dlcnd@users.noreply.github.com>
2019-02-27 11:19:32 +0900

    Update djv.sh

--- scripts/root/djv.sh
+++ scripts/root/djv.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+if [[ $EUID -ne 0 ]]; then
+	echo "This script must be run as Root" 1>&2
+	exit 1
+fi
 yum -y remove DJV #기존의 DJV 프로그램삭제
 cd /tmp #tmp 경로로 이동해서 프로그램 설치
 wget https://sourceforge.net/projects/djv/files/djv-stable/1.2.5/DJV-1.2.5-1.x86_64.rpm
