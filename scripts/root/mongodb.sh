bfac096f92d7f443f12f890015d443fda5f06f9b
dlcnd <32660722+dlcnd@users.noreply.github.com>
2019-02-27 11:23:48 +0900

    Update mongodb.sh

--- scripts/root/mongodb.sh
+++ scripts/root/mongodb.sh
@@ -10,7 +10,8 @@ echo "gpgcheck=1" >> /etc/yum.repos.d/mongodb-org.repo
 echo "enabled=1" >> /etc/yum.repos.d/mongodb-org.repo
 echo "gpgkey=https://www.mongodb.org/static/pgp/server-4.0.asc" >> /etc/yum.repos.d/mongodb-org.repo
 
-yum install -y mongodb-org
-firewall-cmd --zone=public --add-port=27017/tcp --permanet
+yum -y install mongodb-org
+firewall-cmd --zone=public --add-port=27017/tcp --permanent
 firewall-cmd --reload
+chkconfig mongod on
 service mongod start
